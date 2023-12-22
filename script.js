0337820dc99c7352c4a1ccc3939799f6a8632849
Jacob Roberts <jacobroberts.dev@gmail.com>
2023-12-22 09:22:27 -0600

    Add basic styles. Update UI

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Get elements
 const rockButton = document.querySelector(".rock");
 const paperButton = document.querySelector(".paper");
 const scissorsButton = document.querySelector(".scissors");
@@ -10,33 +11,34 @@ const allButtons = document.querySelectorAll("button");
 let computerScore = 0;
 let playerScore = 0;
 let playerSelection;
+let computerSelection;
 
 // Get computer selection
 function getComputerSelection() {
-  let options = ["Rock", "Paper", "Scissors"];
+  const options = ["Rock", "Paper", "Scissors"];
   return options[Math.floor(Math.random() * options.length)];
 }
 
 // Play a round of the game
 function gameRound() {
-  let computerSelection = getComputerSelection();
+  computerSelection = getComputerSelection();
 
   if (computerSelection == playerSelection) {
     resultMessage.textContent = `Tie! Both players chose ${computerSelection}`;
   } else if (
-    (computerSelection === "Rock" && playerSelection == "Scissors") ||
-    (computerSelection === "Paper" && playerSelection == "Rock") ||
-    (computerSelection === "Scissors" && playerSelection == "Paper")
+    (computerSelection === "Rock" && playerSelection === "Scissors") ||
+    (computerSelection === "Paper" && playerSelection === "Rock") ||
+    (computerSelection === "Scissors" && playerSelection === "Paper")
   ) {
     computerScore++;
-    resultMessage.textContent = `Computer wins! ${computerSelection} beats ${playerSelection}`;
+    resultMessage.textContent = `Computer wins! Computer's ${computerSelection} beat your ${playerSelection}`;
   } else if (
-    (playerSelection == "Rock" && computerSelection == "Scissors") ||
-    (playerSelection == "Paper" && computerSelection == "Rock") ||
-    (playerSelection == "Scissors" && computerSelection == "Paper")
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")
   ) {
     playerScore++;
-    resultMessage.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+    resultMessage.textContent = `You win! Your ${playerSelection} beat computer's ${computerSelection}`;
   }
   scoreBoard.textContent = `Computer: ${computerScore} You: ${playerScore}`;
 }
@@ -46,24 +48,29 @@ function endGame() {
   for (let button of allButtons) {
     button.classList.toggle("hidden");
   }
+
+  if (computerScore === 5) {
+    resultMessage.textContent = `Game Over! Computer Wins with ${computerSelection}!`;
+  } else if (playerScore === 5) {
+    resultMessage.textContent = `Game Over! You Win with ${playerSelection}!`;
+  }
 }
 
-//Reset scores and display
+//Reset the scores and display
 function resetScores() {
   playerScore = 0;
   computerScore = 0;
-  scoreBoard.textContent = `Computer: ${computerScore} You: ${playerScore}`;
+  scoreBoard.textContent = `Score: 0`;
 }
 
 // Check for a winner (first to 5 points)
 function checkForWinner() {
   if (playerScore === 5 || computerScore === 5) {
-    resultMessage.textContent = `Game Over!`;
     endGame();
   }
 }
 
-// Event listeners
+//Player selection events
 function playerChoice(button, choice) {
   button.addEventListener("click", () => {
     playerSelection = choice;
@@ -76,8 +83,9 @@ playerChoice(rockButton, "Rock");
 playerChoice(paperButton, "Paper");
 playerChoice(scissorsButton, "Scissors");
 
+// Reset Game event
 resetButton.addEventListener("click", () => {
   resetScores();
   endGame();
-  resultMessage.textContent = ``;
+  resultMessage.textContent = `First to 5 points wins!`;
 });
